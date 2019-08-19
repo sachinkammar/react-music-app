0c3c97e65a92d7c721ff281e2a3f95e9534b2ce6
Sachin Kammar <sachin.kammar@platfi.com>
2019-08-20 00:31:14 +0530

    Updates

---
+++ precache-manifest.333b1369420ecc6f8efd6cbaa1483ceb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dad479fc5ba94b9a6ade2a841b073fe",
+    "url": "/sachinkammar/react-music-app/index.html"
+  },
+  {
+    "revision": "aedf1b7362afbe540c87",
+    "url": "/sachinkammar/react-music-app/static/css/2.6afed8d2.chunk.css"
+  },
+  {
+    "revision": "f3afe7439cba6f83ae74",
+    "url": "/sachinkammar/react-music-app/static/css/main.ac6da1f7.chunk.css"
+  },
+  {
+    "revision": "aedf1b7362afbe540c87",
+    "url": "/sachinkammar/react-music-app/static/js/2.a2f89e86.chunk.js"
+  },
+  {
+    "revision": "f3afe7439cba6f83ae74",
+    "url": "/sachinkammar/react-music-app/static/js/main.a06e1549.chunk.js"
+  },
+  {
+    "revision": "63fbe2c32e23a7bf111e",
+    "url": "/sachinkammar/react-music-app/static/js/runtime~main.f7802243.js"
+  },
+  {
+    "revision": "640c207ce5fa6169a2b02c73797353b1",
+    "url": "/sachinkammar/react-music-app/static/media/Gotham Rounded Bold.640c207c.woff"
+  },
+  {
+    "revision": "8d8762ba7f993ed830957d37c269ea83",
+    "url": "/sachinkammar/react-music-app/static/media/Gotham Rounded Light Italic.8d8762ba.woff"
+  },
+  {
+    "revision": "6a4f95c7904e0d0c0f64a60a6ab4e386",
+    "url": "/sachinkammar/react-music-app/static/media/Gotham Rounded Light.6a4f95c7.woff"
+  },
+  {
+    "revision": "b2779c11381e264fc97a3f6b642d8552",
+    "url": "/sachinkammar/react-music-app/static/media/Gotham Rounded Medium Italic.b2779c11.woff"
+  },
+  {
+    "revision": "29c9cff4805f2c09a2b62133f2f8a4e4",
+    "url": "/sachinkammar/react-music-app/static/media/Gotham Rounded Medium.29c9cff4.woff"
+  }
+]);
